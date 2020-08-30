2c77819737a61d3a63ffe359fbdd9f5754b9de26
azezmohammed <muhamadaziz047@gmail.com>
2020-08-30 16:42:36 +0700

    Javascript Array of object

--- learn/self-taught.js
+++ learn/self-taught.js
@@ -97,4 +97,79 @@
 // console.log(nomor.nomorDua);
 // console.log(nomor.nomorTiga);
 
-// Looping Pada Object
+// // Looping Pada Object
+// const muhamadAziz = {
+//   pekerjaan: "Software Engineer",
+//   lokasi: "Jakarta, Indonssia",
+//   pengalaman: {
+//     pendidikan: {
+//       pati: "SMPN 6 Pati",
+//       yogyakarta: "Universitas Terbuka",
+//     },
+//   },
+// };
+
+// for (let data in muhamadAziz) {
+//   console.log(muhamadAziz[data]);
+// }
+
+// for (let pendidikan in muhamadAziz.pengalaman.pendidikan) {
+//   console.log(muhamadAziz.pengalaman.pendidikan[pendidikan]);
+// }
+
+// Array of object
+
+let buah = [
+  {
+    nama: "Apple",
+    warna: "hijau",
+    namaLatin: "Malus sylvestris",
+    tipeBiji: "single seed",
+  },
+  {
+    nama: "Banana",
+    warna: "kuning",
+    namaLatin: "Musa paradisiaca",
+    tipeBiji: "single seed",
+  },
+  {
+    nama: "Blueberry",
+    warna: "ungu",
+    namaLatin: "Vaccinium corymbosum",
+    tipeBiji: "single seed",
+  },
+  {
+    nama: "Cherry",
+    warna: "merah",
+    namaLatin: "Prunus apetala",
+    tipeBiji: "Double seed",
+  },
+  {
+    nama: "Lemon",
+    warna: "Kuning",
+    namaLatin: "Citrus limon",
+    tipeBiji: "Single seed",
+  },
+  {
+    nama: "Mango",
+    warna: "Kuning",
+    namaLatin: "Citrus reticulata",
+    tipeBiji: "Double seed",
+  },
+  {
+    nama: "Orange",
+    warna: "Kuning",
+    namaLatin: "Malus sylvestris",
+    tipeBiji: "Single seed",
+  },
+  {
+    nama: "Pear",
+    warna: "kuning kehijauan",
+    namaLatin: "Pyrus caucasica",
+    tipeBiji: "Double seed",
+  },
+];
+
+buah.forEach((daftarBuah) => {
+  console.log(daftarBuah);
+});
---
+++ script.js
@@ -0,0 +1,54 @@
+let buah = [
+  {
+    nama: "Apple",
+    warna: "hijau",
+    namaLatin: "Malus sylvestris",
+    tipeBiji: "single seed",
+  },
+  {
+    nama: "Banana",
+    warna: "kuning",
+    namaLatin: "Musa paradisiaca",
+    tipeBiji: "single seed",
+  },
+  {
+    nama: "Blueberry",
+    warna: "ungu",
+    namaLatin: "Vaccinium corymbosum",
+    tipeBiji: "single seed",
+  },
+  {
+    nama: "Cherry",
+    warna: "merah",
+    namaLatin: "Prunus apetala",
+    tipeBiji: "Double seed",
+  },
+  {
+    nama: "Lemon",
+    warna: "Kuning",
+    namaLatin: "Citrus limon",
+    tipeBiji: "Single seed",
+  },
+  {
+    nama: "Mango",
+    warna: "Kuning",
+    namaLatin: "Citrus reticulata",
+    tipeBiji: "Double seed",
+  },
+  {
+    nama: "Orange",
+    warna: "Kuning",
+    namaLatin: "Malus sylvestris",
+    tipeBiji: "Single seed",
+  },
+  {
+    nama: "Pear",
+    warna: "kuning kehijauan",
+    namaLatin: "Pyrus caucasica",
+    tipeBiji: "Double seed",
+  },
+];
+
+buah.forEach((daftarBuah) => {
+  console.log(daftarBuah);
+});
